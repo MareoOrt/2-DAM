7939b8fbc219078480bb9fae15e73146f9938240
MareoOrt <98015233+MareoOrt@users.noreply.github.com>
2023-09-21 13:39:57 +0200

    Carpetas del curso
    Se añadió las carpetas del curso 23-24

---
+++ Desarrollo de Interfaces/Evaluacion Inical - Mario Ortuñez Sanz/Evaluación inicial.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.Serialization;
+
+namespace EvaluaciónInicial
+{
+    internal class Evaluacion
+    {
+        static void Main(string[] args)
+        {
+            DateTime hoy = DateTime.Now;
+
+            int hora = hoy.Hour;
+
+            if (hora < 14)
+            {
+                Console.WriteLine("Buenos dias");
+            }
+            else
+            {
+                Console.WriteLine("Buenas tardes");
+            }
+
+            Console.WriteLine("Son las " + hoy);
+
+            string frase = Console.ReadLine();
+
+            string fraseinversa = "";
+
+            int numeroLetras = 0;
+
+            foreach (char letra in frase)
+            {
+                fraseinversa = letra + fraseinversa;
+                numeroLetras++;
+            }
+            if (numeroLetras != 0)
+            {
+
+                Console.WriteLine("Tu frase fue: " + frase);
+                Console.WriteLine("Invertida es: " + fraseinversa);
+
+                Console.WriteLine("La frase tenia " + numeroLetras);
+            }
+            else
+            {
+                Console.WriteLine("No escribistes nada");
+            }
+        }
+
+    }
+}
---
+++ Desarrollo de Interfaces/Evaluacion Inical - Mario Ortuñez Sanz/Program.cs
@@ -0,0 +1,15 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//namespace HolaMundo
+//{
+//    internal class Programa
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hola mundo cruel");
+//            string frase = Console.ReadLine();
+//            DateTime dateTime = DateTime.Now.Date;
+//        }
+        
+//    }
+//}
+
