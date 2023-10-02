fe3f14c3608be98001bac03feafb5141de4a6b1e
MareoOrt <98015233+MareoOrt@users.noreply.github.com>
2023-10-02 13:45:13 +0200

    Clase 02/10/2023
    Clase 02/10/2023

---
+++ Desarrollo de Interfaces/1º Evaluacion/WpfAgua/Deposito.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAgua
+{
+    internal class Deposito
+    {
+        public Deposito()
+        {
+            Cantidad = 10;
+        }
+        #region otra forma
+        /*
+        private int _cantidad;
+
+        public int Cantidad { get; set; }
+
+        */
+        #endregion
+
+        public event EventHandler DepositoVacio;
+        public event EventHandler CantidadCambiada;
+
+        private int cantidad;
+
+        public int Cantidad
+        {
+            get { return cantidad; }
+            set 
+            {
+                #region con excepciones
+                /*
+                if (value > 10)
+                {
+                    throw new ArgumentException("Valor superior de 10");
+                }
+                if (value == 0)
+                {
+                    throw new ArgumentException("Depósito vacio");
+                }
+                */
+                #endregion
+
+                cantidad = value;
+            
+                if(cantidad == 0)
+                {
+                    DepositoVacio?.Invoke(this, new EventArgs());
+                }
+            }
+        }
+
+    }
+}
---
+++ Desarrollo de Interfaces/1º Evaluacion/WpfAgua/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAgua
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Deposito deposito = new Deposito();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Pruebas();
+
+            deposito.DepositoVacio += Deposito_DepositoVacio;
+        }
+
+        private void Deposito_DepositoVacio(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+            MessageBox.Show(ex.Message, "ERROR",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+        }
+
+        public void Pruebas()
+        {
+            deposito = new Deposito();
+            try
+            {
+                deposito.Cantidad = 10;
+            }
+            catch (Exception ex)
+            {
+                //throw new Exception("ERROR!!" +  ex.Message
+                MessageBox.Show(ex.Message, "ERROR",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private void botPulsar_Click(object sender, RoutedEventArgs e)
+        {
+            deposito.Cantidad --;
+            lblCantidad.Content = deposito.Cantidad;
+
+        }
+    }
+}
