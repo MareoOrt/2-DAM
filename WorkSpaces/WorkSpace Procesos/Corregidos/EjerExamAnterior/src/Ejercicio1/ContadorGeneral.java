9147c26a00a0052bc10abdc9cd8795a33a610321
MareoOrt <98015233+MareoOrt@users.noreply.github.com>
2023-10-18 13:48:41 +0200

    Clase 18/10/2023
    Clase 18/10/2023

---
+++ WorkSpaces/WorkSpace Procesos/Corregidos/EjerExamAnterior/src/Ejercicio1/ContadorGeneral.java
@@ -0,0 +1,18 @@
+package Ejercicio1;
+
+public class ContadorGeneral {
+	private int contador;
+
+	public int getContador() {
+		return contador;
+	}
+
+	public void setContador(int contador) {
+		this.contador = contador;
+	}
+
+	public ContadorGeneral(int contador) {
+		super();
+		this.contador = contador;
+	}
+}
