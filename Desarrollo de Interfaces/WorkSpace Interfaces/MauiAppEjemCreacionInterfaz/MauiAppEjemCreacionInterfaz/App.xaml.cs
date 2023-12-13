3107500e985c9a3fbb0eb7b655a6e000f27993e2
MareoOrt <98015233+MareoOrt@users.noreply.github.com>
2023-12-13 21:25:01 +0100

    En casa 13/12/2023
    En casa 13/12/2023

---
+++ Desarrollo de Interfaces/WorkSpace Interfaces/MauiAppEjemCreacionInterfaz/MauiAppEjemCreacionInterfaz/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppEjemCreacionInterfaz
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
